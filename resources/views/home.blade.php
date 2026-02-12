ddcb71fe8c59541acb870716c25997c57e6dcf83
skyler157 <leahgitau007@gmail.com>
2026-02-12 15:06:50 +0300

    Fix favicon on production (remove empty ico, use svg icon links)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ReelSnap</title>
-    <link rel="icon" type="image/svg+xml" href="{{ asset('reelsnap-icon.svg') }}">
+    <link rel="icon" href="/reelsnap-icon.svg?v=2" sizes="any" type="image/svg+xml">
+    <link rel="shortcut icon" href="/reelsnap-icon.svg?v=2" type="image/svg+xml">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700;800&display=swap" rel="stylesheet">
